178951539ea1fe1a09058adeaefb5984714676f0
César Suárez <csuarez@aplyca.com>
2022-10-18 18:37:00 -0500

    unificados archivos debido a error de CORS

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import Dispatcher from "./dispatcher.js";
-
-$(document).ready( function () {
-    let table = $("#processesTable");
-    let addButton = $("#addRandomProcess");
-    let dispatcher = new Dispatcher(table, addButton);
-    dispatcher.init();
-} );
